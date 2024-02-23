d1aae27930e408f46ace7c7490a1449bb9d586e8
LouieAtFetch <146202371+LouieAtFetch@users.noreply.github.com>
2024-02-23 14:30:30 -0600

    [DO-2957] Set allowed characters for hostname (#3)
    * Restrict Characters

--- Sources/XCMetricsClient/Machine Name Reader/MachineNameReader.swift
+++ Sources/XCMetricsClient/Machine Name Reader/MachineNameReader.swift
@@ -37,17 +37,12 @@ class HashedMacOSMachineNameReader: MachineNameReader {
         if encrypted {
             return Host.current().localizedName?.md5()
         }
-        return removeLocalizedSingleQuotesAndSpaces(from: Host.current().localizedName)
+        return normalizing(Host.current().localizedName)
     }
 
-    func removeLocalizedSingleQuotesAndSpaces(from input: String) -> String {
-        // Define a set of localized variations of single quotes
-        let localizedSingleQuotes: Set<Character> = [
-            "‘", "’", "`", "ʹ", "′", "‛", "❛", "❜", "＇", "'"
-        ]
-
-        // Iterate through each character in the input string
-        return input.filter { !localizedSingleQuotes.contains($0) && !$0.isWhitespace }
+    private func normalizing(_ input: String) -> String {
+        let allowedChars = Set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+        return String(input.filter { allowedChars.contains($0) })
     }
 }
 
